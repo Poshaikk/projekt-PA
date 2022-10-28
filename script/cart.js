d3acf93a553bebc676e9866bcdfa774edb096a2c
Poshaikk <116850394+Poshaikk@users.noreply.github.com>
2022-10-28 21:58:46 +0200

    koniec zrobilem lol fajna strona

--- script/cart.js
+++ script/cart.js
@@ -1,4 +1,5 @@
 function loadCart(){
+    var totalPayment = 0
     var shoppingListItems = document.getElementById("shopping-list-items")
     var shoppingListPrices = document.getElementById("shopping-list-prices")
     var productAmounts = GetStorage("product_amounts")
@@ -13,13 +14,19 @@ function loadCart(){
             sListPrice.innerHTML = (`${GetStorage(productList[i])[1]}zł x ${productAmounts[i]} = ${GetStorage(productList[i])[1]*productAmounts[i]}zł`).toString()
             shoppingListItems.appendChild(sListItem)
             shoppingListPrices.appendChild(sListPrice)
+            totalPayment+=GetStorage(productList[i])[1]*productAmounts[i]
         }
     }
+    console.log(totalPayment)
     if(element){
         document.getElementById("empty-cart").style = "display: block;"
         document.getElementsByClassName("shopping-list-wrapper")[0].style = "justify-content: center; align-items: center;"
-
+        document.getElementById("final-price").value = "0zł"
     }
+    else{
+        document.getElementById("final-price").value = totalPayment+"zł"
+    }
+
 }
 function GetStorage(sessionKey){
     var answer = JSON.parse(sessionStorage.getItem(sessionKey))
